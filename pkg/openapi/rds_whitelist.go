e1064a1c6e71ffc213e97c8a4256ab517664877b
liangchenghao <liangchenghao.lch@alibaba-inc.com>
2023-03-26 20:12:05 +0800

    fit rds high security mode

--- pkg/openapi/rds_whitelist.go
+++ pkg/openapi/rds_whitelist.go
@@ -7,85 +7,117 @@ import (
 
 func (r *RdsWhitelistOperator) Whitelists(rdsId string) ([]rds.DBInstanceIPArray, error) {
 
+	var lists []rds.DBInstanceIPArray
 	req := rds.CreateDescribeDBInstanceIPArrayListRequest()
 	req.RegionId = RegionID
 	req.DBInstanceId = rdsId
 
-	resp, err := r.DescribeDBInstanceIPArrayList(req)
-	if err != nil {
-		return nil, err
+	networkTypes := []string{"MIX", "Classic", "VPC"}
+	for _, networkType := range networkTypes {
+		req.WhitelistNetworkType = networkType
+		resp, err := r.DescribeDBInstanceIPArrayList(req)
+		if err != nil {
+			return nil, err
+		}
+
+		for _, ipArray := range resp.Items.DBInstanceIPArray {
+			lists = append(lists, ipArray)
+		}
 	}
-	return resp.Items.DBInstanceIPArray, nil
+
+	return lists, nil
 }
 
 func (r *RdsWhitelistOperator) CreateWhitelist(rdsId, podIP, whitelistName string) error {
 
-	req := rds.CreateModifySecurityIpsRequest()
-	req.RegionId = RegionID
-	req.DBInstanceId = rdsId
-	req.SecurityIPType = "ipv4"
-	req.SecurityIps = podIP
-	req.WhitelistNetworkType = "MIX"
-	req.ModifyMode = "Append"
-
-	if whitelistName == "" {
-		req.DBInstanceIPArrayName = "rds_default"
-	} else {
-		req.DBInstanceIPArrayName = whitelistName
+	lists, err := r.GetWhitelist(rdsId, whitelistName)
+	if err != nil {
+		return err
 	}
 
-	_, err := r.ModifySecurityIps(req)
-
-	if err != nil && ParseErrorMessage(err.Error()).ErrorCode == "SecurityIPList.Duplicate" {
-		return nil
+	if lists == nil {
+		return fmt.Errorf("no such whitelist %s under rdsId %s", whitelistName, rdsId)
 	}
 
-	return err
+	for _, list := range lists {
+		req := rds.CreateModifySecurityIpsRequest()
+		req.RegionId = RegionID
+		req.DBInstanceId = rdsId
+		req.SecurityIPType = "ipv4"
+		req.SecurityIps = podIP
+		req.WhitelistNetworkType = list.WhitelistNetworkType
+		req.ModifyMode = "Append"
+
+		if whitelistName == "" {
+			req.DBInstanceIPArrayName = "rds_default"
+		} else {
+			req.DBInstanceIPArrayName = whitelistName
+		}
+
+		_, err := r.ModifySecurityIps(req)
+
+		if err != nil {
+			if ParseErrorMessage(err.Error()).ErrorCode == "InvalidInstanceIp.Duplicate" {
+				continue
+			}
+			return err
+		}
+	}
+	return nil
 }
 
-func (r *RdsWhitelistOperator) GetWhitelist(rdsId, whitelistName string) (*rds.DBInstanceIPArray, error) {
+func (r *RdsWhitelistOperator) GetWhitelist(rdsId, whitelistName string) ([]rds.DBInstanceIPArray, error) {
 
 	lists, err := r.Whitelists(rdsId)
 	if err != nil {
 		return nil, err
 	}
 
+	var whitelists []rds.DBInstanceIPArray
 	for _, list := range lists {
 		if list.DBInstanceIPArrayName == whitelistName {
-			return &list, nil
+			whitelists = append(whitelists, list)
 		}
 	}
 
-	return nil, nil
+	return whitelists, nil
 }
 
 func (r *RdsWhitelistOperator) DeleteWhitelist(rdsId, whitelistName, podIP string) error {
 
-	list, err := r.GetWhitelist(rdsId, whitelistName)
+	lists, err := r.GetWhitelist(rdsId, whitelistName)
 	if err != nil {
 		return err
 	}
 
-	if list == nil {
+	if lists == nil {
 		return fmt.Errorf("no such whitelist %s under rdsId %s", whitelistName, rdsId)
 	}
 
-	req := rds.CreateModifySecurityIpsRequest()
-	req.RegionId = RegionID
-	req.DBInstanceId = rdsId
-	req.SecurityIps = list.SecurityIPList
-	req.WhitelistNetworkType = list.WhitelistNetworkType
-	req.SecurityIps = podIP
-	req.ModifyMode = "Delete"
+	for _, list := range lists {
+		req := rds.CreateModifySecurityIpsRequest()
+		req.RegionId = RegionID
+		req.DBInstanceId = rdsId
+		req.WhitelistNetworkType = list.WhitelistNetworkType
+		req.SecurityIps = podIP
+		req.ModifyMode = "Delete"
 
-	if whitelistName == "" {
-		req.DBInstanceIPArrayName = "default"
-	} else {
-		req.DBInstanceIPArrayName = whitelistName
+		if whitelistName == "" {
+			req.DBInstanceIPArrayName = "default"
+		} else {
+			req.DBInstanceIPArrayName = whitelistName
+		}
+
+		_, err = r.ModifySecurityIps(req)
+		if err != nil {
+			if ParseErrorMessage(err.Error()).ErrorCode == "InvalidSecurityIPs.NotFound" {
+				continue
+			}
+			return err
+		}
 	}
 
-	_, err = r.ModifySecurityIps(req)
-	return err
+	return nil
 }
 
 //func RefactorRdsWhitelistName(name string) string {
